3435e169acf45c4cd4ac8077d37c80690c8f6cf6
Naol Denko <naoldenko@outlook.com>
2025-02-08 03:17:07 -0500

    feat: add favicon configuration and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,35 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Denko Group',
   description: 'Tech, Mental Health, and Entertainment',
+  icons: {
+    icon: [
+      {
+        url: '/favicon_io/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png'
+      },
+      {
+        url: '/favicon_io/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
+        url: '/favicon_io/favicon.ico',
+        sizes: 'any'
+      }
+    ],
+    apple: {
+      url: '/favicon_io/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png'
+    },
+    other: [
+      {
+        rel: 'manifest',
+        url: '/favicon_io/site.webmanifest'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
